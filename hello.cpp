b34524886bf2b09471b042ee8bf6e4f5e26910bc
Dan Stephenson <ispyhumanfly@gmail.com>
2023-12-13 16:21:26 -0500

    Fix.

--- hello.cpp
+++ hello.cpp
@@ -1,20 +1,9 @@
 #include <iostream>
-#include <string> 
-#include <random>
-#include <vector>
 
-std::vector<std::string> languages = {"Java", "Python", "C++", "JavaScript", "C", "SQL",  
-"Go", "Swift", "Kotlin", "Ruby"};
-
-int main() {
+int main() { 
     
-    std::random_device rd;
-    std::mt19937 gen(rd());
-    std::shuffle(languages.begin(), languages.end(), gen);
+    std::cout << "Hello You!!@#!" << std::endl; return 0;
 
-    for(int i = 0; i < 10; i++) {
-        std::cout << "Hello World! in " << languages[i] << std::endl;
-    }
-
-    return 0;
+    std::cout << "Hello World!" << std::endl; return 0;
+    
 }
