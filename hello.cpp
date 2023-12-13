f975cda235dedcfbb3cf9795d7924ee8d7db8991
Dan Stephenson <ispyhumanfly@gmail.com>
2023-12-13 16:19:50 -0500

    Now in random languages!

--- hello.cpp
+++ hello.cpp
@@ -1,7 +1,20 @@
 #include <iostream>
+#include <string> 
+#include <random>
+#include <vector>
 
-int main() { 
+std::vector<std::string> languages = {"Java", "Python", "C++", "JavaScript", "C", "SQL",  
+"Go", "Swift", "Kotlin", "Ruby"};
+
+int main() {
     
-    std::cout << "Hello World!" << std::endl; return 0;
-    
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::shuffle(languages.begin(), languages.end(), gen);
+
+    for(int i = 0; i < 10; i++) {
+        std::cout << "Hello World! in " << languages[i] << std::endl;
+    }
+
+    return 0;
 }
